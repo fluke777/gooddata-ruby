705f332d206205514d7f5f0bd084877467998f72
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-05-19 22:52:16 -0700

    Variable

--- lib/gooddata/models/metadata/variable.rb
+++ lib/gooddata/models/metadata/variable.rb
@@ -1,16 +1,22 @@
 # encoding: UTF-8
 
 require_relative 'metadata'
+require 'pmap'
 
 module GoodData
   class UserFilter
 
     def initialize(data)
+      @dirty = false
       @json = data
     end
 
+    def related_uri
+      @json['related']
+    end
+
     def related
-      uri = @json['related']
+      uri = related_uri
       level == :project ? GoodData::Project[uri] : GoodData::AccountSettings.new(GoodData.get(uri))
     end
 
@@ -26,14 +32,59 @@ module GoodData
     def type
       @json['type'].to_sym
     end
-    
 
     def expression
       @json['expression']
     end
 
+    def expression=(expr)
+      @dirty = true
+      @json['expression'] = expr
+    end
+
+    def uri
+      @json['uri']
+    end
+
     def pretty_expression
       SmallGoodZilla.pretty_print(expression)
+    end
+
+    def to_hash
+      @json
+    end
+
+    def delete
+      GoodData.delete(uri)
+    end
+
+    def save
+      GoodData.post(uri, { :variable => @json })
+    end
+
+  end
+
+  class MandatoryUserFilter < MdObject
+    class << self
+      def [](id, options = {})
+        if id == :all
+          all(options)
+        else
+          super
+        end
+      end
+
+      def all(options={})
+        vars = GoodData.get(GoodData.project.md['query'] + '/userfilters/')['query']['entries']
+        vars.map do |a|
+          data = GoodData.get(a['link'])
+          GoodData::UserFilter.new(
+            "expression" => data['userFilter']['content']['expression'],
+            "level"=> :user,
+            "type" => :filter
+          )
+        end
+      end
     end
   end
 
@@ -55,13 +106,14 @@ module GoodData
       end
     end
 
-    def values
+    def find_values(*context)
+      uris = context.map { |obj| obj.respond_to?(:uri) ? obj.uri : obj }
       x = {
           "variablesSearch" => {
               "variables" => [
                   uri
               ],
-              "context" => []
+              "context" => uris
           }
       }
       data = GoodData.post("/gdc/md/#{project.pid}/variables/search", x)
@@ -72,11 +124,11 @@ module GoodData
     end
 
     def project_values
-      values.select { |v| v.level == :project }
+      find_values.select { |v| v.level == :project }
     end
 
     def user_values
-      values.select { |v| v.level == :user }
+      find_values.select { |v| v.level == :user }
     end
 
     def type
@@ -88,7 +140,6 @@ module GoodData
     end
   end
 end
-
 
 module GoodData
   module UserFilterBuilder
@@ -159,6 +210,97 @@ module GoodData
       end
     end
 
+    def self.maqlify_filters(result, options = {})
+      only_existing_users = options[:only_existing_users] || true
+
+      users_lookup = GoodData.project.users.reduce({}) do |a, e|
+        a[e.login] = e
+        a
+      end
+
+      if only_existing_users
+        result.each do |filter|
+          login = filter[:login]
+          fail "User #{login} is not part of the project and variable #{filter} cannot be resolved" unless users_lookup.key?(login)
+        end
+      end
+
+      result.map do |filter|
+        # fail "User could"
+        login = filter[:login]
+        filters = filter[:filters]
+        expressions = filters.map do |filter|
+          values = filter[:values]
+          label = GoodData::DisplayForm[filter[:label][:uri]]
+          element_uris = values.map {|v| label.find_value_uri(v)}
+          "[#{label.attribute.uri}] IN (#{ element_uris.map { |e| '[' + e + ']' }.join(', ') })"
+        end
+        expression = expressions.join(' AND ')
+
+        GoodData::UserFilter.new(
+          {
+            "related" => users_lookup[login].uri,
+            "level"=> :user,
+            "expression" => expression,
+            "type" => :filter
+          })
+      end
+    end
+
+    def self.resolve_variable_user_fiters(user_filters, var)
+      vals = var.user_values
+      project_vals_lookup = vals.reduce({}) do |a, e|
+        a[e.related_uri] = e
+        a
+      end
+
+      to_update = []
+      to_delete = []
+      to_create = []
+      user_filters.each do |val|
+        user_uri = val.related_uri
+        if project_vals_lookup.key?(user_uri)
+          project_val = project_vals_lookup[user_uri]
+          if val.expression != project_val.expression
+            project_val.expression = val.expression
+            to_update << project_val
+          end
+        else
+          to_create << val
+        end
+      end
+      [to_create, to_delete, to_update]
+    end
+
+    def self.execute_variables(filters, var, options = {})
+      dry_run = options[:dry_run]
+      user_filters = maqlify_filters(filters, :only_existing_users => true)
+      to_create, to_delete, to_update = resolve_variable_user_fiters(user_filters, var)
+
+      return [to_create, to_delete, to_update] if dry_run
+
+      to_delete.each &:delete
+      data = to_create.map(&:to_hash).map { |var_val| var_val.merge({:prompt => var.uri })}
+      data.each_slice(200) do |slice|
+        GoodData.post("/gdc/md/#{GoodData.project.obj_id}/variables/user", ({:variables => slice}))
+      end
+      to_update.each do |value|
+        value.save
+      end
+      
+      [to_create, to_delete, to_update]
+    end
+
+    def self.resolve_mandatory_user_fiters(filters)
+      
+    end
+
+    def self.execute_mufs(filters, name)
+      user_filters = maqlify_filters(filters, :only_existing_users => false)
+      to_create, to_delete, to_update = resolve_mandatory_user_fiters(user_filters)
+      # binding.pry
+    end
+
     # - filters resolve
     # - if unable to resolve fail
     # - if able to resolve move on
@@ -168,64 +310,23 @@ module GoodData
   end
 end
 
-
 def example
-  x = "login,country,age\nsvarovsky@gooddata.com,Tomas,14\nsvarovsky@gooddata.com,Petr,19\nsvarovsky@gooddata.com,Tomas,30"
-  result = GoodData::UserFilterBuilder::get_filters(x, {
+  GoodData.logging_on
+  var = GoodData::Variable.all[1]
+  x = "login,division,age\nsvarovsky@gooddata.com,hr,14\nsvarovsky@gooddata.com,\"rnd, eng\",19\n"
+  filters = GoodData::UserFilterBuilder::get_filters(x, {
     :type => :filter,
     :labels => [
-      {:label => {:uri => "/gdc/md/j5wt8v2tl077r21r568l19vx16pq2qal/obj/27"}, :column => 'country'}
+      {:label => {:uri => "/gdc/md/j5wt8v2tl077r21r568l19vx16pq2qal/obj/25"}, :column => 'division'}
     ]
   })
-
-  # result.[
-  #   {
-  #     :login=>"tomas@gooddata.com",
-  #     :filters=> [
-  #       {:label=>{:uri=>"label/34"}, :values=>["US"]},
-  #       {:label=>{:uri=>"label/99"}, :values=>["14"]}
-  #     ]
-  #   },
-  #   {
-  #     :login=>"petr@gooddata.com",
-  #     :filters => [
-  #       {:label=>{:uri=>"label/34"}, :values=>["US", "KZ"]},
-  #       {:label=>{:uri=>"label/99"}, :values=>["19", "30"]}
-  #     ]
-  #   }
-  # ]
-
-  users_lookup = GoodData.project.users.reduce({}) do |a, e|
-    a[e.login] = e
-    a
-  end
-
-  var_values = result.map do |filter|
-    # fail "User could"
-    login = filter[:login]
-    fail "User #{login} is not part of the project and variable cannot be resolved" unless users_lookup.key?(login)
-    related_uri = users_lookup[login].uri
-
-    filters = filter[:filters]
-    expressions = filters.map do |filter|
-      values = filter[:values]
-      label = GoodData::DisplayForm[filter[:label][:uri]]
-      element_uris = values.map {|v| label.find_value_uri(v)}
-      "[#{label.attribute.uri}] IN (#{ element_uris.map { |e| '[' + e + ']' }.join(', ') })"
-    end
-
-    expression = expressions.join(' AND ')
-
-    GoodData::UserFilter.new(
-      {
-        "related" => related_uri,
-        "level"=> :user,
-        "expression" => expression,
-        "type" => :filter
-      })
-  end
-  
-  var = GoodData::Variable.all[1]
-  vals = var.user_values
   binding.pry
+  GoodData::UserFilterBuilder.execute_mufs(filters, "some var")
+end
+
+class Hash
+  def slice(*keys)
+    keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
+    keys.each_with_object(self.class.new) { |k, hash| hash[k] = self[k] if has_key?(k) }
+  end
 end
