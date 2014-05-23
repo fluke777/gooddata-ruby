975c43b7f39c2d22cb689ca4c18d9928975fd8a3
Tomas Svarovsky <svarovsky@gooddata.com>
2014-05-23 12:18:26 -0700

    added over to

--- lib/gooddata/models/metadata/variable.rb
+++ lib/gooddata/models/metadata/variable.rb
@@ -16,7 +16,7 @@ module GoodData
 
     def related
       uri = related_uri
-      level == :project ? GoodData::Project[uri] : GoodData::AccountSettings.new(GoodData.get(uri))
+      level == :project ? GoodData::Project[uri] : GoodData::Profile.new(GoodData.get(uri))
     end
 
     def variable
@@ -159,6 +159,7 @@ module GoodData
 
     def self.get_filters(file, options={})
       values = get_values(file, options)
+      binding.pry
       UserFilterBuilder.reduce_results(values)
     end
 
@@ -166,7 +167,7 @@ module GoodData
       type = options[:type]
       labels = options[:labels]
 
-      results = if labels.size == 1 && labels.first.key?(:column) == false
+      if labels.size == 1 && labels.first.key?(:column) == false
         read_line_wise(file, options)
       else
         read_column_wise(file, options)
@@ -177,7 +178,7 @@ module GoodData
       labels = options[:labels]
       memo = {}
 
-      CSV.parse(file, :headers => true) do |e|
+      CSV.foreach(file, :headers => true, :return_headers => false) do |e|
         login = e[0]
         labels.each do |label|
           column = label[:column]
@@ -186,7 +187,9 @@ module GoodData
           memo[login] = [] unless memo.key?(login)
           memo[login] << {
             :label => label[:label],
-            :values => values
+            :values => values,
+            :over => label[:over],
+            :to => label[:to]
           }
         end
       end
@@ -197,13 +200,15 @@ module GoodData
       label = options[:labels].first
       memo = {}
 
-      CSV.parse(file) do |e|
+      CSV.foreach(file, :headers => false, :return_headers => false) do |e|
         login = e.first
         values = e[1..-1]
         memo[login] = [] unless memo.key?(login)
         memo[login] << {
           :label => label[:label],
-          :values => values
+          :values => values,
+          :over => label[:over],
+          :to => label[:to]
         }
       end
       memo
@@ -214,7 +219,7 @@ module GoodData
     end
 
     def self.collect_labels(data)
-      data.group_by {|x| x[:label]}.map {|l, v| {:label => l, :values => UserFilterBuilder.collect_values(v)}}
+      data.group_by {|x| [x[:label], x[:over], x[:to]]}.map {|l, v| {:label => l[0], :over => l[1], :to => l[2], :values => UserFilterBuilder.collect_values(v)}}
     end
 
     def self.collect_values(data)
@@ -231,7 +236,6 @@ module GoodData
         a[e.login] = e
         a
       end
-
       if only_existing_users
         result.each do |filter|
           login = filter[:login]
@@ -239,15 +243,59 @@ module GoodData
         end
       end
 
-      result.map do |filter|
+      # attribtues_cache = result.reduce({}) do |a, e|
+      #   e[:filters].map do |label|
+      #     a[label[:over]] = GoodData::Attribute[label[:over]] unless a.key?(label[:over])
+      #     a[label[:to]] = GoodData::Attribute[label[:to]] unless a.key?(label[:to])
+      #   end
+      #   a
+      # end
+
+      labels_cache = result.reduce({}) do |a, e|
+        e[:filters].map do |label|
+          a[label[:label][:uri]] = GoodData::DisplayForm[label[:label][:uri]] unless a.key?(label[:label][:uri])
+        end
+        a
+      end
+      small_labels = labels_cache.values.find_all {|label| label.values_count < 100000}
+      cache = small_labels.reduce({}) do |a, e|
+        lookup = e.values(:limit => 1000000).reduce({}) do |a1, e1|
+          a1[e1[:value]] = e1[:uri]
+          a1
+        end
+        a[e.uri] = lookup
+        a
+      end
+
+      errors = []
+      results = result.map do |filter|
         # fail "User could"
         login = filter[:login]
         filters = filter[:filters]
         expressions = filters.map do |filter|
           values = filter[:values]
-          label = GoodData::DisplayForm[filter[:label][:uri]]
-          element_uris = values.map {|v| label.find_value_uri(v)}
-          "[#{label.attribute.uri}] IN (#{ element_uris.map { |e| '[' + e + ']' }.join(', ') })"
+          label = labels_cache[filter[:label][:uri]]
+          element_uris = values.map do |v|
+            begin
+              if cache.key?(label.uri)
+                if cache[label.uri].key?(v)
+                  cache[label.uri][v]
+                else
+                  fail
+                end
+              else
+                label.find_value_uri(v)
+              end
+            rescue
+              errors << [label, v]
+              ""
+            end
+          end
+          if filter[:over] && filter[:to]
+            "[#{label.attribute_uri}] IN (#{ element_uris.map { |e| '[' + e + ']' }.join(', ') }) OVER [#{filter[:over]}] TO [#{filter[:to]}]"
+          else
+            "[#{label.attribute_uri}] IN (#{ element_uris.map { |e| '[' + e + ']' }.join(', ') })"
+          end
         end
         expression = expressions.join(' AND ')
 
@@ -259,6 +307,9 @@ module GoodData
             "type" => :filter
           })
       end
+      binding.pry
+      fail errors unless errors.empty?
+      results
     end
 
     def self.resolve_variable_user_fiters(user_filters, vals)
@@ -306,7 +357,6 @@ module GoodData
       to_create, to_delete, to_update = resolve_variable_user_fiters(user_filters, var.user_values)
 
       return [to_create, to_delete, to_update] if dry_run
-
       to_delete.each &:delete
       data = to_create.map(&:to_hash).map { |var_val| var_val.merge({:prompt => var.uri })}
       data.each_slice(200) do |slice|
@@ -392,15 +442,16 @@ module GoodData
 end
 
 def example
-  var = GoodData::Variable.all[1]
-  x = "login,division,age\nsvarovsky@gooddata.com,hr,14\nsvarovsky@gooddata.com,\"Tomas\",19\n"
-  filters = GoodData::UserFilterBuilder::get_filters(x, {
+  GoodData.logging_on
+  var = GoodData::Variable.all[1];
+  label = GoodData::DisplayForm["/gdc/md/8z2c3wx15novcptlafq908aylaxqz468/obj/13"];
+  filters = GoodData::UserFilterBuilder::get_filters('portfolioCompanies.csv', {
     :type => :filter,
     :labels => [
-      {:label => {:uri => "/gdc/md/om15m97we27svj0t6csvaggwlnn8qwwj/obj/210"}, :column => 'division'}
+      {:label => {:uri => "/gdc/md/8z2c3wx15novcptlafq908aylaxqz468/obj/13"}, :to => "/gdc/md/8z2c3wx15novcptlafq908aylaxqz468/obj/13", :over => "/gdc/md/8z2c3wx15novcptlafq908aylaxqz468/obj/13" }
     ]
-  })
-  GoodData::UserFilterBuilder.execute_variables(filters, var)
+  });
+  GoodData::UserFilterBuilder.execute_variables(filters, var, :dry_run => true)
 end
 
 def muf_example
@@ -416,11 +467,11 @@ def muf_example
   # })
   # GoodData::UserFilterBuilder.execute_mufs(filters, :dry_run => true)
   
-  l = GoodData::Attribute[198].labels[1]
+  label = GoodData::Attribute.find_first_by_title('Dev').label_by_name('Id')
   filters = [
-    ["svarovsky@gooddata.com", l.uri, "1", "3"]
+    ["svarovsky@gooddata.com", label.uri, "1", "3"]
   ]
-  GoodData::UserFilterBuilder.execute_mufs(filters, :dry_run => true)
+  GoodData::UserFilterBuilder.execute_mufs(filters)
   
 end
 
