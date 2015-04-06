35bc2d08bbb5ab6ac32f72159d8756fb93c8d5c2
Tomas Svarovsky <svarovsky@gooddata.com>
2015-04-06 16:41:21 -0700

    Blueprint should work with descriptions

--- spec/unit/models/from_wire_spec.rb
+++ spec/unit/models/from_wire_spec.rb
@@ -42,11 +42,11 @@ describe GoodData::Model::FromWire do
   end
 
   it "should enable sorting" do
-    pending("UAAA")
+    skip("UAAA")
   end
 
   it "should allow defining date dimensions" do
-    pending('UAAA')
+    skip('UAAA')
   end
 
   it "should generate the same thing it parsed" do
@@ -115,6 +115,12 @@ describe GoodData::Model::FromWire do
         :gd_type=>"GDC.text",
         :default_label => true
       }]
+  end
+
+  it "should be able to parse description from both attributes and facts" do
+    expect(@blueprint.find_dataset('opportunity').anchor[:description]).to eq 'This is opportunity attribute description'
+    expect(@blueprint.find_dataset('stage_history').facts.find {|f| f[:name] == 'stage_velocity'}[:description]).to eq 'Velocity description'
+    expect(@blueprint.find_dataset('opp_owner').attributes.find {|f| f[:name] == 'region'}[:description]).to eq 'Owner Region description'
   end
 
   it "should be able to deal with fiscal dimensions with weird names" do
