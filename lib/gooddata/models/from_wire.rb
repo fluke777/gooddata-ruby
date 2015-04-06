80a108bffbc78d6fe221c49f0ad75eae16e2835d
Tomas Svarovsky <svarovsky@gooddata.com>
2015-04-06 16:41:21 -0700

    Blueprint should work with descriptions
    Conflicts:
    	spec/data/wire_test_project.json

--- lib/gooddata/models/from_wire.rb
+++ lib/gooddata/models/from_wire.rb
@@ -106,6 +106,7 @@ module GoodData
             f[:name] = fact['fact']['identifier'].split('.').last
             f[:folder] = fact['fact']['folder'] if fact['fact']['folder']
             f[:title] = fact['fact']['title'] if fact['fact']['title'] != fact['fact']['identifier'].split('.').last.titleize
+            f[:description] = fact['fact']['description'] if fact['fact']['description']
             f[:gd_data_type] = fact['fact']['dataType'] if fact['fact'].key?('dataType')
           end
         end
@@ -122,6 +123,7 @@ module GoodData
           l[:folder] = attribute['folder'] if attribute['folder'] && (type == 'attribute' || type == 'anchor')
           l[:name] = label['label']['identifier'].split('.').last
           l[:title] = label['label']['title'] if label['label']['title'] != label['label']['identifier'].split('.').last.titleize
+          l[:description] = attribute['description'] if ['attribute', 'anchor'].include?(type) && attribute['description']
           l[:gd_data_type] = label['label']['dataType'] if label['label'].key?('dataType')
           l[:gd_type] = label['label']['type'] if label['label'].key?('type')
           l[:default_label] = true if default_label == label['label']['identifier']
